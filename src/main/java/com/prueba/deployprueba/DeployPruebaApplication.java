1b50c0e3554ed4a54afd5e845646a70caa925227
otniel <palacioso@globalhitss.com>
2023-11-28 16:13:31 -0600

    first commit

---
+++ src/main/java/com/prueba/deployprueba/DeployPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.deployprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/deployprueba/DeployPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.deployprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
