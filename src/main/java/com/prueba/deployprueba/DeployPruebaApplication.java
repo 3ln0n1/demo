db24c4a07b8ae30b4976bbbe8c0aa8583ddf3f06
otniel <palacioso@globalhitss.com>
2023-11-28 17:22:48 -0600

    mensaje

--- src/main/java/com/prueba/deployprueba/DeployPruebaApplication.java
+++ src/main/java/com/prueba/deployprueba/DeployPruebaApplication.java
@@ -2,10 +2,18 @@ package com.prueba.deployprueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DeployPruebaApplication {
-
+	
+	@GetMapping("/mensaje")
+	public String mensaje() {
+		return "mensaje desde api";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeployPruebaApplication.class, args);
 	}
